94d31ee1d84a55f588155137eae7aff24de11297
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-06-24 22:49:39 +0800

    Change from LaTeX to HTML

--- cmd/archive.sh
+++ cmd/archive.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+node html2pdf.js
 FILE="Chan_Yat_Fu_Jacky"
-mkdir -p archives && cp -r ${FILE}.pdf archives/${FILE}_$(date +"%Y_%m_%d").pdf && git add archives/${FILE}_$(date +"%Y_%m_%d").pdf
+mkdir -p archives && cp -r out/${FILE}.pdf archives/${FILE}_$(date +"%Y_%m_%d").pdf && git add archives/${FILE}_$(date +"%Y_%m_%d").pdf
---
+++ html2pdf.js
@@ -0,0 +1,16 @@
+const puppeteer = require("puppeteer");
+const path = require("path");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Resolve the relative path to the index.html
+  const filePath = path.resolve(__dirname, "./index.html");
+  const fileUrl = `file://${filePath}`;
+
+  await page.goto(fileUrl, { waitUntil: "networkidle2" });
+  await page.pdf({ path: "out/Chan_Yat_Fu_Jacky.pdf", format: "A4" });
+
+  await browser.close();
+})();
