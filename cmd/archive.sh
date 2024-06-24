2ae557633e25c277c025f3e98cdb810ba59b32e1
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-06-25 03:08:50 +0800

    Add up-to-date checker

--- cmd/archive.sh
+++ cmd/archive.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-node html2pdf.js
 FILE="Chan_Yat_Fu_Jacky"
 mkdir -p archives && cp -r out/${FILE}.pdf archives/${FILE}_$(date +"%Y_%m_%d").pdf && git add archives/${FILE}_$(date +"%Y_%m_%d").pdf
---
+++ cmd/insert-commit-hash.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+GIT_HASH=$(git rev-parse --short HEAD)
+HTML_FILE="Chan_Yat_Fu_Jacky.html"
+awk -v hash="$GIT_HASH" 'BEGIN { RS = "id=\"check-update-anchor\"" }
+NR == 1 { print }
+NR > 1 {
+  sub("href=\"[^\"]*\"", "href=\"https://resume.yatfuchan.com/?from=" hash "\"")
+  gsub(/\n/, "")
+  print "id=\"check-update-anchor\"" $0
+}' "$HTML_FILE" > "$HTML_FILE.tmp" && mv "$HTML_FILE.tmp" "$HTML_FILE"
+prettier --write Chan_Yat_Fu_Jacky.html
+sed -i.bak "s|<div id=\"prev-commit-hash\" hidden>.*</div>|<div id=\"prev-commit-hash\" hidden>$GIT_HASH</div>|" "$HTML_FILE"
+
+
+git add "$HTML_FILE"
