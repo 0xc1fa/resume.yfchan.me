1b056efdcb1ec72afd12f2a9fac60be207b0ad31
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-07-10 20:47:57 +0800

    Change site name to Route 51 compatitable

--- cmd/insert-commit-hash.sh
+++ cmd/insert-commit-hash.sh
@@ -5,7 +5,7 @@ HTML_FILE="Chan_Yat_Fu_Jacky.html"
 awk -v hash="$GIT_HASH" 'BEGIN { RS = "id=\"check-update-anchor\"" }
 NR == 1 { print }
 NR > 1 {
-  sub("href=\"[^\"]*\"", "href=\"http://058264110432-resume-site-ap-east-1.s3-website.ap-east-1.amazonaws.com/?from=" hash "\"")
+  sub("href=\"[^\"]*\"", "href=\"http://resume.yfchan.me/?from=" hash "\"")
   gsub(/\n/, "")
   print "id=\"check-update-anchor\"" $0
 }' "$HTML_FILE" > "$HTML_FILE.tmp" && mv "$HTML_FILE.tmp" "$HTML_FILE"
