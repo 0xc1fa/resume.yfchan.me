085af876b46cab02a485e6f8cdc346e5345f0563
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-07-11 04:39:06 +0800

    Change from raw HTML to Next.js

--- cmd/html2pdf.js
+++ cmd/html2pdf.js
@@ -1,14 +1,10 @@
 const puppeteer = require("puppeteer");
-const path = require("path");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  const rootDir = process.cwd();
-
-  const filePath = path.resolve(rootDir, "./out/index.html");
-  const fileUrl = `file://${filePath}`;
+  const fileUrl = `http://resume.yfchan.me`;
 
   await page.goto(fileUrl, { waitUntil: "networkidle2" });
   await page.pdf({ path: "out/Chan_Yat_Fu_Jacky.pdf", format: "A4" });
